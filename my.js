e888f26c3cc6bd5c328b7bb8a37d1b0b6d48a0d9
Errin Johnson <errin@alchemymomentum.com>
2015-02-02 18:32:19 -0500

    tweeks to css, wellness.html and add my.js script project

--- my.js
+++ my.js
@@ -1,11 +1,29 @@
-// comment section
-//console.log("Hello from myscript.js");
-//console.log("Hello again!"); // This is not needed
-/*
-var name = prompt("What is your name?");
-alert("Hello " + name); 
+/*Extra Credit
+Fizz Buzz
 
-var Lname = prompt("Type Your Last Name Here");
-var Fname= prompt("Type Your First Name Here");
-alert("Hello " + Fname + " " + Lname)
-console.log("The user's full name is " + Fname + " " + Lname) */
+Write a program that loops through the numbers 1 through 100. Each number should be printed to the console, using console.log(). However, if the number is a multiple of 3, don't print the number, instead print the word "fizz". If the number is a multiple of 5, print "buzz" instead of the number. If it is a multiple of 3 and a multiple of 5, print "fizzbuzz" instead of the number.
+
+Hint. Use loops and if/else statments. In javascript the % is the modulo, or remainder operator. a % b evaluates to the remainder of a divided by b. 11 % 3 is equal to 2.*/
+
+console.log("Before");
+
+ 
+for (var counter = 100; counter >= 1; counter = counter - 1) {
+    if (counter % 3 == 0) {
+        console.log("fizz");
+    }
+        
+    if (counter % 5 == 0) {
+        console.log("buzz");
+    }
+    if (counter % 5 == 0 && counter % 3 == 0) {
+        console.log("fizzbuzz");
+    }
+    else {
+        console.log(counter);
+    }
+    
+} 
+console.log("After");
+
+
