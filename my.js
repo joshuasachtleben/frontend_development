29e2caf13ed62a3484a58e6c8347c01523af7629
Errin Johnson <errin@alchemymomentum.com>
2015-02-02 12:31:51 -0500

    removed header and footer border properties, added box-shadow property, add image to footer

---
+++ my.js
@@ -0,0 +1,11 @@
+// comment section
+//console.log("Hello from myscript.js");
+//console.log("Hello again!"); // This is not needed
+/*
+var name = prompt("What is your name?");
+alert("Hello " + name); 
+
+var Lname = prompt("Type Your Last Name Here");
+var Fname= prompt("Type Your First Name Here");
+alert("Hello " + Fname + " " + Lname)
+console.log("The user's full name is " + Fname + " " + Lname) */
